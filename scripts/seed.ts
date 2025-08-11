256e8b2b7647e418ff0bdae4afdac22c441f34a2
siphonhata <siphonhata@gmail.com>
2025-08-11 10:47:54 +0200

    feat: dotenv

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,5 +1,8 @@
+import { config } from 'dotenv'
+config()
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '../lib/database.types'
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
